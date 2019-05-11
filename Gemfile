e6a424cf35bb8d7fbbbe4146ed8782e30eb19821
Florin Ionce <ionce.florin@gmail.com>
2019-05-11 14:16:22 +0300

    Add faker gem and database seeds

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem 'rails', '~> 5.2.3'
 gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
+
+gem 'faker', git: 'https://github.com/stympy/faker.git', branch: 'master'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  user = User.create(
+    email: Faker::Internet.unique.email,
+    name: Faker::Movies::StarWars.unique.character
+  )
+
+  3.times do
+    Blogpost.create(
+      user: user,
+      title: Faker::Movies::StarWars.quote,
+      body: Faker::Lorem.paragraphs(rand(2..8)).join('.')
+    )
+  end
+end
