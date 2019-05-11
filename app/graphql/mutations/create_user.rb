587d1ef4907d01ee5129e0ff099f26768330e88e
Florin Ionce <ionce.florin@gmail.com>
2019-05-11 15:39:31 +0300

    Add create user mutation

---
+++ app/graphql/mutations/create_user.rb
@@ -0,0 +1,25 @@
+module Mutations
+  class CreateUser < BaseMutation
+    argument :name, String, required: true
+    argument :email, String, required: true
+
+    field :user, Types::UserType, null: false
+    field :errors, [String], null: false
+
+    def resolve(name:, email:)
+      user = User.new(name: name, email: email)
+      if user.save
+        {
+          user: user,
+          errors: [],
+        }
+      else
+        # Failed save, return the errors to the client
+        {
+          user: nil,
+          errors: user.errors.full_messages
+        }
+      end
+    end
+  end
+end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,6 +2,7 @@ module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
     field :name, String, null: true
+    field :email, String, null: true
     field :blogposts, [Types::BlogpostType], null: true
     field :blogposts_count, Integer, null: true
 
